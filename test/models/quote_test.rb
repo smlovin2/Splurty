1f1c99fbdc03d6ee4bffa256000e892fca055adb
Sean Lovinger <lovingersean01@gmail.com>
2015-12-16 19:08:14 +0000

    Added the uniqe_tag method and a test for it

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "unique_tag" do
+    quote = Quote.create(:author => 'Sean Lovinger', :saying => 'Never put twinkies on your pizza!')
+    expected = 'SL#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
   end
 end
