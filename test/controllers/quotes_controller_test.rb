2fdc3b66d1ebdd61588771ca38459937e0993ed2
Sean Lovinger <lovingersean01@gmail.com>
2015-12-17 00:04:51 +0000

    Added tests for creating a valid and invalid quote

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -11,4 +11,26 @@ class QuotesControllerTest < ActionController::TestCase
     get :show, :id => 'OMG'
     assert_response :not_found
   end
+
+  test "create a quote success" do
+    assert_difference 'Quote.count' do
+      post :create, :quote => {
+        :author => 'Yoda',
+        :saying => 'Do. Or do not. There is no try'
+      }
+    end
+
+    assert_redirected_to root_path
+  end
+
+  test "create a quote validation error" do
+    assert_no_difference "Quote.count" do
+      post :create, :quote => {
+        :author => '',
+        :saying => '',
+      }
+
+      assert_redirected_to root_path
+    end
+  end
 end
